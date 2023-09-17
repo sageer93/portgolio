e68be358b43b78a8dae9945109baea2220a89cf7
sageer93 <128065898+sageer93@users.noreply.github.com>
2023-09-16 20:50:54 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const withPWA = require("next-pwa")({
 });
 
 module.exports = withPWA({
-  reactStrictMode: false,
+  reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
